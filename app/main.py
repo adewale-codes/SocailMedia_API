3c3d42433c207ec6dcbebe56bf95945189a797dd
adewale-codes <adewaleabiola78@gmail.com>
2024-01-06 10:41:50 +0100

    added vote route and change database details to env format

--- app/main.py
+++ app/main.py
@@ -1,52 +1,29 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
-from .routers import post, user, auth
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+from . import models
+from .database import engine
+from .routers import post, user, auth, vote
+from .config import settings
 
 app = FastAPI()
 
+origins = ["*"]
 
-
-while True:
-    
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='*abi1oLA',     cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful")
-        break
-    except Exception as error:
-        print("Connection to database failed")
-        print("Error: ", error)
-        time.sleep(2)
-
-my_posts = [{"title": "title of post one", "content": "content of post one", "id": 1}, {"title": "favorite food", "content": "i like food", "id": 2}]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-        
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello world"}
-
-
+def root():
+    return {"message": "Hello World pushing out to ubuntu"}
--- app/oauth2.py
+++ app/oauth2.py
@@ -1,21 +1,34 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas, database, models
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from .config import settings
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+# SECRET_KEY
+# Algorithm
+# Expriation time
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
 
-    def verify_access_token(token: str, credentials_exception):
+
+def verify_access_token(token: str, credentials_exception):
 
     try:
 
